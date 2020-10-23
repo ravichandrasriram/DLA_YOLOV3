a148dee7a22460c0aaddc776a6ab32754d26b141
ravichandrasriram <ravichandrasriram@gmail.com>
2020-10-23 18:09:30 +0530

    Update train_detector.py

--- 7_yolov3/lib/train_detector.py
+++ 7_yolov3/lib/train_detector.py
@@ -287,7 +287,7 @@ class Detector():
             self.system_dict["params"]["adam"] = False;
         self.system_dict["fixed_params"]["hyp"]["lr0"] = lr;
         self.system_dict["params"]["rect"] = False;
-        self.system_dict["params"]["resume"] = True;
+        self.system_dict["params"]["resume"] = False;
         self.system_dict["params"]["nosave"] = False;
         self.system_dict["params"]["notest"] = False;
         self.system_dict["params"]["evolve"] = evolve;
@@ -315,7 +315,7 @@ class Detector():
 
 
         #Device Setup
-        #self.system_dict["params"]["weights"] = last if self.system_dict["params"]["resume"] else self.system_dict["params"]["weights"]
+        self.system_dict["params"]["weights"] = last if self.system_dict["params"]["resume"] else self.system_dict["params"]["weights"]
         self.system_dict["local"]["device"] = torch_utils.select_device(self.system_dict["params"]["device"], 
                                                                         apex=self.system_dict["params"]["mixed_precision"], 
                                                                         batch_size=self.system_dict["params"]["batch_size"])
